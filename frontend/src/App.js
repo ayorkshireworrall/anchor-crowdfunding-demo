d004e39379b0be385dbf32b6c494499e478f1a62
ayorkshireworrall <a.worrall2512@googlemail.com>
2023-01-12 10:42:09 +0000

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,199 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, AnchorProvider, web3, utils, BN } from '@coral-xyz/anchor';
+import { Buffer } from 'buffer';
+
+import idl from './idl.json';
+
+window.Buffer = Buffer;
+
+const programId = new PublicKey(idl.metadata.address);
+const network = clusterApiUrl('devnet');
+const opts = {
+  preflightCommitment: 'processed'
+}
+const { SystemProgram } = web3;
+
+const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [campaigns, setCampaigns] = useState([]);
+  const initialCampaignData = {
+    name: '',
+    description: ''
+  }
+  const [campaignData, setCampaignData] = useState(initialCampaignData)
+
+  const handleInputChange = (input, e) => {
+    let data = { ...campaignData }
+    data[input] = e.target.value
+    setCampaignData(data)
+  }
+
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new AnchorProvider(connection, window.solana, opts.preflightCommitment);
+    return provider;
+  }
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana && solana.isPhantom) {
+        console.log('Phantom wallet found!');
+        const response = await solana.connect({
+          onlyIfTrusted: true
+        });
+        console.log('Connected with public key: ', response.publicKey.toString());
+        setWalletAddress(response.publicKey.toString());
+      } else {
+        alert("Solana object not found! Get a Phantom wallet")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const connectWallet = async () => {
+    console.log('Connecting wallet');
+    const { solana } = window;
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with public key: ', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  }
+
+  const getCampaigns = async () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = getProvider();
+    const program = new Program(idl, programId, provider);
+
+    Promise.all(
+      (await connection.getProgramAccounts(programId)).map(
+        async (campaign) => ({
+          ...(await program.account.campaign.fetch(campaign.pubkey)),
+          pubkey: campaign.pubkey,
+        })
+      )
+    ).then((campaigns) => {
+      console.log('Campaigns found: ', campaigns);
+      setCampaigns(campaigns)
+    });
+  }
+
+  const createCampaign = async (name, description) => {
+    console.log(`Creating a campaign with name ${name} and description ${description}`)
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+      console.log(program.programId.toString())
+      const [campaign] = PublicKey.findProgramAddressSync([
+        utils.bytes.utf8.encode('campaign'),
+        provider.wallet.publicKey.toBuffer(),
+        utils.bytes.utf8.encode(name)
+      ], program.programId);
+
+      console.log(campaign.toString())
+      await program.methods
+        .create(name, description)
+        .accounts({
+          campaign,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId
+        })
+        .rpc();
+      console.log('Created a new campaign with address: ', campaign.toString());
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const donate = async (campaignKey, amount) => {
+    console.log(`A donation of ${amount} has been requested to the campaign with ID ${campaignKey}`)
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+
+      await program.methods
+        .donate(new BN(amount * web3.LAMPORTS_PER_SOL))
+        .accounts({
+          campaign: campaignKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId
+        })
+        .rpc();
+      console.log(`${amount} has been donated to the campaign with ID ${campaignKey}`)
+    } catch (error) {
+      console.error('Error donating: ', error)
+    }
+  }
+
+  const withdraw = async (campaignKey, amount) => {
+    console.log('About to withdraw from campaign ', campaignKey.toString());
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+
+      await program.methods
+        .withdraw(new BN(amount * web3.LAMPORTS_PER_SOL))
+        .accounts({
+          campaign: campaignKey,
+          user: provider.wallet.publicKey
+        })
+        .rpc();
+      console.log('Successfully withdrawn from the campaign')
+    } catch (error) {
+      console.error('Error withdrawing from campaign: ', error);
+    }
+  }
+
+  const renderNotConnectedContainer = () => {
+    return <button onClick={connectWallet}>Connect to Wallet</button>
+  }
+
+  const renderConnectedContainer = () => {
+    return (
+      <>
+        <p>Name:</p>
+        <input onChange={e => handleInputChange('name', e)} value={campaignData['name']} />
+        <p>Description:</p>
+        <input onChange={e => handleInputChange('description', e)} value={campaignData['description']} />
+        <button onClick={() => createCampaign(campaignData['name'], campaignData['description'])}>Create a Campaign</button>
+        <button onClick={getCampaigns}>Get a list of campaigns</button>
+        <br />
+        {campaigns.map(campaign => {
+          return (
+            <>
+              <p>Campaign ID: {campaign.pubkey.toString()}</p>
+              <p>Balance: {(campaign.amountDonated / web3.LAMPORTS_PER_SOL).toString()}</p>
+              <p>Name: {campaign.name}</p>
+              <p>Description: {campaign.description}</p>
+              <button onClick={() => donate(campaign.pubkey, 0.2)}>Click To Donate</button>
+              <button onClick={() => withdraw(campaign.pubkey, 0.2)}>Click To Withdraw</button>
+            </>
+          )
+        })}
+      </>
+    )
+  }
+
+
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    }
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  return (
+    <div className='AppContainer'>
+      {!walletAddress && renderNotConnectedContainer()}
+      {walletAddress && renderConnectedContainer()}
+    </div>
+  )
+}
+
+export default App;
---
+++ programs/crowdfunding/src/lib.rs
@@ -0,0 +1,120 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7gBnxsoGc1rgpqhCM6KF1t7ThdqcBj3Jen5nxoAbE5K5");
+
+#[program]
+pub mod crowdfunding {
+
+    use super::*;
+
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        let bump = *ctx.bumps.get("campaign").ok_or(CrowdfundError::CannotGetBump)?;
+
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *ctx.accounts.user.key;
+        campaign.bump = bump;
+        Ok(())
+    }
+
+    // _name variable passed in for seed in campaign context, otherwise not useds
+    pub fn withdraw(ctx: Context<Withdraw>, _name: String, amount: u64) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+        require!(
+            campaign.admin == *user.key,
+            CrowdfundError::IncorrectProgramId
+        );
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+        require!(
+            **campaign.to_account_info().lamports.borrow() - rent_balance > amount,
+            CrowdfundError::InsufficientFunds
+        );
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += amount;
+        Ok(())
+    }
+
+    // _name variable passed in for seed in campaign context, otherwise not used
+    pub fn donate(ctx: Context<Donate>, _name: String, amount: u64) -> Result<()> {
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.user.key(), 
+            &ctx.accounts.campaign.key(), 
+            amount
+        );
+        anchor_lang::solana_program::program::invoke(
+            &ix, 
+            &[
+                ctx.accounts.user.to_account_info(),
+                ctx.accounts.campaign.to_account_info(),
+            ]
+        )?;
+        (&mut ctx.accounts.campaign).amount_donated += amount;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct Create<'info> {
+    // The seeds value is required because account needs to be a "program-derived" account (handled differently in tests)
+    #[account(
+        init, 
+        payer=user, 
+        space=9000, 
+        seeds=[b"campaign", user.key.as_ref(), name.as_bytes().as_ref()],
+        bump
+    )]
+    campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct Withdraw<'info> {
+    #[account(
+        mut,
+        seeds=[b"campaign", user.key.as_ref(), name.as_bytes().as_ref()],
+        bump
+    )]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct Donate<'info> {
+    #[account(
+        mut,
+        seeds=[b"campaign", user.key.as_ref(), name.as_bytes().as_ref()],
+        bump = campaign.bump
+    )]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct Campaign {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub amount_donated: u64,
+    pub bump: u8,
+}
+
+#[error_code]
+pub enum CrowdfundError {
+    #[msg("Insufficient funds")]
+    InsufficientFunds,
+    #[msg("User is not the campaign admin")]
+    IncorrectProgramId,
+    #[msg("Cannot get the bump.")]
+    CannotGetBump
+}
